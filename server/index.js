b9e20d67d1a43836bb7e721b77d52d20bb6624e8
mustafamubashir03 <114705378+mustafamubashir03@users.noreply.github.com>
2024-11-08 23:28:48 +0500

    port for railway

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../frontend/dist/index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server has been started");
 });
