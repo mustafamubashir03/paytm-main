792a0ff84887e5a6f447a8f0c28bf10fdddfc2d0
mustafamubashir03 <114705378+mustafamubashir03@users.noreply.github.com>
2024-11-08 16:44:23 +0500

    Final Deployed Version

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { join } = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mainRouter = require("./routes/index.js");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use("/api/v1", mainRouter);
+
+// Serve static files from the frontend directory
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Render the client app
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server has been started");
+});
